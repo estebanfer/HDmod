af18c4e170518620b9910b69caa6073af1adb093
estebanfer <estebanfer2004@gmail.com>
2022-04-09 19:18:16 -0300

    locals and unused on acid

--- lib/entities/acid.lua
+++ lib/entities/acid.lua
@@ -1,7 +1,7 @@
 local module = {}
 
-ACID_POISONTIME = 270 -- For reference, HD's was 3-4 seconds
-acid_tick = ACID_POISONTIME
+local ACID_POISONTIME = 270 -- For reference, HD's was 3-4 seconds
+local acid_tick = ACID_POISONTIME
 
 function module.init()
 	acid_tick = ACID_POISONTIME
@@ -11,11 +11,11 @@ set_callback(function()
 	-- Worm LEVEL
 	if state.theme == THEME.EGGPLANT_WORLD then
 		-- Acid damage
-		for i, player in ipairs(players) do
+		for _, player in ipairs(players) do
 			-- local spelunker_mov = get_entity(player):as_movable()
 			local spelunker_swimming = test_flag(player.more_flags, 11)
 			local poisoned = player:is_poisoned()
-			x, y, l = get_position(player.uid)
+			local x, y, l = get_position(player.uid)
 			if spelunker_swimming and player.health ~= 0 and not poisoned then
 				if acid_tick <= 0 then
 					spawn(ENT_TYPE.ITEM_ACIDSPIT, x, y, l, 0, 0)
@@ -35,7 +35,7 @@ set_callback(function()
 	if state.theme == THEME.EGGPLANT_WORLD then
 		set_interval(function()
             local fx = get_entities_by_type(ENT_TYPE.FX_WATER_SURFACE)
-            for i,v in ipairs(fx) do
+            for _,v in ipairs(fx) do
                 local x, y, l = get_position(v)
                 if math.random() < 0.003 then
                     spawn_entity(ENT_TYPE.ITEM_ACIDBUBBLE, x, y, l, 0, 0)

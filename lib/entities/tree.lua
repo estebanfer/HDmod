7d1f0f4bd28713067f5810a6d1d29bf1e00b9e33
estebanfer <estebanfer2004@gmail.com>
2022-04-11 14:02:03 -0300

    locals on tree

--- lib/entities/tree.lua
+++ lib/entities/tree.lua
@@ -5,22 +5,22 @@ local module = {}
 -- Use the following methods for a starting point:
 
 -- HD-style tree decorating methods
-function decorate_tree(e_type, p_uid, side, y_offset, radius, right)
+local function decorate_tree(e_type, p_uid, side, y_offset, radius, right)
 	if p_uid == 0 then return 0 end
-	p_x, p_y, p_l = get_position(p_uid)
-	branches = get_entities_at(e_type, 0, p_x+side, p_y, p_l, radius)
-	branch_uid = 0
+	local p_x, p_y, p_l = get_position(p_uid)
+	local branches = get_entities_at(e_type, 0, p_x+side, p_y, p_l, radius)
+	local branch_uid = 0
 	if #branches == 0 then
 		branch_uid = spawn_entity_over(e_type, p_uid, side, y_offset)
 		if e_type == ENT_TYPE.DECORATION_TREE then
-			branch_e = get_entity(branch_uid)
+			local branch_e = get_entity(branch_uid)
 			branch_e.animation_frame = 87+12*math.random(2)
 		end
 	else
 		branch_uid = branches[1]
 	end
 	-- flip if you just created it and it's a 0x100 and it's on the left or if it's 0x200 and on the right.
-	branch_e = get_entity(branch_uid)
+	local branch_e = get_entity(branch_uid)
 	if branch_e ~= nil then
 		-- flipped = test_flag(branch_e.flags, ENT_FLAG.FACING_LEFT)
 		if (#branches == 0 and branch_e.type.search_flags == 0x100 and side == -1) then -- to flip branches
@@ -38,11 +38,11 @@ function module.onlevel_decorate_trees()
 		options.hd_og_tree_spawn == false
 	) then
 		-- remove tree vines
-		treeParts = get_entities_by_type(ENT_TYPE.FLOOR_TREE_BRANCH)
+		local treeParts = get_entities_by_type(ENT_TYPE.FLOOR_TREE_BRANCH)
 		for _, treebranch in ipairs(treeParts) do
 			if entity_has_item_type(treebranch, ENT_TYPE.DECORATION_TREE_VINE_TOP) then
-				treeVineTop = entity_get_items_by(treebranch, ENT_TYPE.DECORATION_TREE_VINE_TOP, 0)[1]
-				_x, _y, _l = get_position(treeVineTop)
+				local treeVineTop = entity_get_items_by(treebranch, ENT_TYPE.DECORATION_TREE_VINE_TOP, 0)[1]
+				local _x, _y, _l = get_position(treeVineTop)
 				
 				-- don't kill it if it's the top
 				if (
@@ -63,8 +63,8 @@ function module.onlevel_decorate_trees()
 		-- add branches to tops of trees, add leaf decorations
 		treeParts = get_entities_by_type(ENT_TYPE.FLOOR_TREE_TOP)
 		for _, treetop in ipairs(treeParts) do
-			branch_uid_left = decorate_tree(ENT_TYPE.FLOOR_TREE_BRANCH, treetop, -1, 0, 0.1, false)
-			branch_uid_right = decorate_tree(ENT_TYPE.FLOOR_TREE_BRANCH, treetop, 1, 0, 0.1, false)
+			local branch_uid_left = decorate_tree(ENT_TYPE.FLOOR_TREE_BRANCH, treetop, -1, 0, 0.1, false)
+			local branch_uid_right = decorate_tree(ENT_TYPE.FLOOR_TREE_BRANCH, treetop, 1, 0, 0.1, false)
 			if (
 				feelingslib.feeling_check(feelingslib.FEELING_ID.RESTLESS) == false and
 				feelingslib.feeling_check(feelingslib.FEELING_ID.HAUNTEDCASTLE) == false

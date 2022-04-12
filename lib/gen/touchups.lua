eaa06f3b694f05e6c52dffccaf835031b6affc30
estebanfer <estebanfer2004@gmail.com>
2022-04-12 00:19:22 -0300

    locals on touchups

--- lib/gen/touchups.lua
+++ lib/gen/touchups.lua
@@ -1,16 +1,6 @@
 local module = {}
 
-function module.onlevel_touchups()
-	onlevel_remove_cursedpot()
-	onlevel_remove_mounts()
-	onlevel_hide_yama()
-	onlevel_replace_border_textures()
-	onlevel_removeborderfloor()
-	onlevel_create_impostorlake()
-	onlevel_remove_boulderstatue()
-end
-
-function onlevel_create_impostorlake()
+local function onlevel_create_impostorlake()
 	if feelingslib.feeling_check(feelingslib.FEELING_ID.RUSHING_WATER) then
 		local x, y = 22.5, 88.5--80.5
 		local w, h = 40, 12
@@ -26,7 +16,7 @@ function onlevel_create_impostorlake()
 	end
 end
 
-function onlevel_removeborderfloor()
+local function onlevel_removeborderfloor()
 	if (
 		state.theme == THEME.NEO_BABYLON
 		-- or state.theme == THEME.OLMEC -- Lava touching the void ends up in a crash
@@ -41,7 +31,7 @@ function onlevel_removeborderfloor()
 	end
 end
 
-function onlevel_replace_border_textures()
+local function onlevel_replace_border_textures()
 	if (
 		state.theme == THEME.EGGPLANT_WORLD
 		or state.theme == THEME.VOLCANA
@@ -53,7 +43,7 @@ function onlevel_replace_border_textures()
 		elseif state.theme == THEME.VOLCANA then
 			texture_def.texture_path = "res/hell_border.png"
 		end
-		boneder_texture = define_texture(texture_def)
+		local boneder_texture = define_texture(texture_def)
 
 		local bonebordere = get_entities_by_type(ENT_TYPE.FLOOR_BORDERTILE) -- get all entities of these types
 		for _, boneborder_uid in pairs(bonebordere) do 
@@ -67,12 +57,12 @@ function onlevel_replace_border_textures()
 	end
 end
 
-function onlevel_remove_cursedpot()
-	cursedpot_uids = get_entities_by_type(ENT_TYPE.ITEM_CURSEDPOT)
+local function onlevel_remove_cursedpot()
+	local cursedpot_uids = get_entities_by_type(ENT_TYPE.ITEM_CURSEDPOT)
 	if #cursedpot_uids > 0 and options.hd_og_cursepot_enable == false then
-		xmin, ymin, _, _ = get_bounds()
-		void_x = xmin - 3.5
-		void_y = ymin
+		local xmin, ymin, _, _ = get_bounds()
+		local void_x = xmin - 3.5
+		local void_y = ymin
 		spawn_entity(ENT_TYPE.FLOOR_BORDERTILE, void_x, void_y, LAYER.FRONT, 0, 0)
 		for _, cursedpot_uid in ipairs(cursedpot_uids) do
 			move_entity(cursedpot_uid, void_x, void_y+1, 0, 0)
@@ -80,8 +70,8 @@ function onlevel_remove_cursedpot()
 	end
 end
 
-function onlevel_remove_mounts()
-	mounts = get_entities_by_type({
+local function onlevel_remove_mounts()
+	local mounts = get_entities_by_type({
 		ENT_TYPE.MOUNT_TURKEY
 		-- ENT_TYPE.MOUNT_ROCKDOG,
 		-- ENT_TYPE.MOUNT_AXOLOTL,
@@ -120,7 +110,7 @@ function onlevel_remove_mounts()
 			-- for _, avoidmount in ipairs(avoid) do
 				-- if mount == avoidmount then stop_remove = true end
 			-- end
-			mov = get_entity(mount):as_movable()
+			local mov = get_entity(mount)
 			if test_flag(mov.flags, ENT_FLAG.SHOP_ITEM) == false then --and stop_remove == false then
 				move_entity(mount, 0, 0, 0, 0)
 			end
@@ -128,23 +118,14 @@ function onlevel_remove_mounts()
 	end
 end
 
-function onlevel_remove_boulderstatue()
+local function onlevel_remove_boulderstatue()
 	if state.theme == THEME.ICE_CAVES then
-		boulderbackgrounds = get_entities_by_type(ENT_TYPE.BG_BOULDER_STATUE)
+		local boulderbackgrounds = get_entities_by_type(ENT_TYPE.BG_BOULDER_STATUE)
 		if #boulderbackgrounds > 0 then
 			kill_entity(boulderbackgrounds[1])
 		end
 	end
 end
-
-
--- Use junglespear traps for idol trap blocks and other blocks that shouldn't have post-destruction decorations
-set_post_entity_spawn(function(_entity)
-	_spikes = entity_get_items_by(_entity.uid, ENT_TYPE.LOGICAL_JUNGLESPEAR_TRAP_TRIGGER, 0)
-	for _, _spike in ipairs(_spikes) do
-		kill_entity(_spike)
-	end
-end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOOR_JUNGLE_SPEAR_TRAP)
 
 -- set_pre_entity_spawn(function(ent_type, x, y, l, overlay)
 -- 	-- SORRY NOTHING
@@ -161,7 +142,7 @@ end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOOR_JUNGLE_SPEAR_TRAP)
 -- end, SPAWN_TYPE.ANY, 0, ENT_TYPE.MONS_YAMA)
 -- # TODO: Relocate MONS_YAMA to a better place. Can't move him to back layer, it triggers the slow music :(
 	-- OR: improve hiding him. Could use set_post_entity_spawn.
-function onlevel_hide_yama()
+local function onlevel_hide_yama()
     if state.theme == THEME.EGGPLANT_WORLD then
         -- kill_entity(get_entities_by_type(ENT_TYPE.BG_YAMA_BODY)[1])
         -- for i, yama_floor in ipairs(get_entities_by_type(ENT_TYPE.FLOOR_YAMA_PLATFORM)) do
@@ -176,19 +157,35 @@ function onlevel_hide_yama()
     end
 end
 
+function module.onlevel_touchups()
+	onlevel_remove_cursedpot()
+	onlevel_remove_mounts()
+	onlevel_hide_yama()
+	onlevel_replace_border_textures()
+	onlevel_removeborderfloor()
+	onlevel_create_impostorlake()
+	onlevel_remove_boulderstatue()
+end
 
 -- set_post_entity_spawn(function(entity)
 -- 	entity.flags = set_flag(entity.flags, ENT_FLAG.DEAD)
 -- 	entity:destroy()
 -- end, SPAWN_TYPE.LEVEL_GEN_FLOOR_SPREADING, 0)
 
+-- Use junglespear traps for idol trap blocks and other blocks that shouldn't have post-destruction decorations
+set_post_entity_spawn(function(_entity)
+	local _spikes = entity_get_items_by(_entity.uid, ENT_TYPE.LOGICAL_JUNGLESPEAR_TRAP_TRIGGER, 0)
+	for _, _spike in ipairs(_spikes) do
+		kill_entity(_spike)
+	end
+end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOOR_JUNGLE_SPEAR_TRAP)
 
 set_pre_entity_spawn(function(ent_type, x, y, l, overlay)
-	return spawn_grid_entity(ENT_TYPE.FLOOR_BORDERTILE_METAL, x, y, l, 0, 0)
+	return spawn_grid_entity(ENT_TYPE.FLOOR_BORDERTILE_METAL, x, y, l)
 end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOOR_HORIZONTAL_FORCEFIELD)
 
 set_pre_entity_spawn(function(ent_type, x, y, l, overlay)
-	return spawn_grid_entity(ENT_TYPE.FLOOR_BORDERTILE_METAL, x, y, l, 0, 0)
+	return spawn_grid_entity(ENT_TYPE.FLOOR_BORDERTILE_METAL, x, y, l)
 end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOOR_HORIZONTAL_FORCEFIELD_TOP)
 
 set_post_entity_spawn(function(entity)

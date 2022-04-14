f07d38111cd3a3a1b7ac2f240052ad94b8e9c1c7
estebanfer <estebanfer2004@gmail.com>
2022-04-14 03:39:01 -0300

    wip bacterium and giant frog

--- lib/entities/bacterium.lua
+++ lib/entities/bacterium.lua
@@ -1,26 +1,114 @@
 local celib = require "custom_entities"
 
+local bacterium_texture_id
+do
+    local bacterium_texture_def = TextureDefinition.new()
+    bacterium_texture_def.width = 640
+    bacterium_texture_def.height = 128
+    bacterium_texture_def.tile_width = 128
+    bacterium_texture_def.tile_height = 128
+
+    bacterium_texture_def.texture_path = "bacterium.png"
+    bacterium_texture_id = define_texture(bacterium_texture_def)
+end
+
 local MOVE_MAP = {
     {0, 1},
     {1, 0},
     {0, -1},
     {-1, 0}
 }
+local BACTERIUM_VEL = 0.075
+
+local function play_sound_at_entity(snd, uid, volume)
+    local v = 0.5
+    if volume ~= nil then
+        v = volume
+    end
+    local ent = get_entity(uid)
+    local sound = get_sound(snd)
+    local audio = sound:play(true)
+    local x, y, _ = get_position(ent.uid)
+    local sx, sy = screen_position(x, y)
+    local d = screen_distance(distance(ent.uid, ent.uid))
+    if players[1] ~= nil then
+        d = screen_distance(distance(ent.uid, players[1].uid))
+    end
+    audio:set_parameter(VANILLA_SOUND_PARAM.POS_SCREEN_X, sx)
+    audio:set_parameter(VANILLA_SOUND_PARAM.DIST_CENTER_X, math.abs(sx))
+    audio:set_parameter(VANILLA_SOUND_PARAM.DIST_CENTER_Y, math.abs(sy))
+    audio:set_parameter(VANILLA_SOUND_PARAM.DIST_Z, 0.0)
+    audio:set_parameter(VANILLA_SOUND_PARAM.DIST_PLAYER, d)
+    audio:set_parameter(VANILLA_SOUND_PARAM.VALUE, v)
+    
+    audio:set_pause(false)
+  end
 
 local function get_solid_grid_entity(x, y, l)
     local uid = get_grid_entity_at(x, y, l)
     return test_flag(get_entity_flags(uid), ENT_FLAG.SOLID) and uid or -1
 end
 
+local function spawn_bacterium_rubble(x, y, l, amount)
+    for _=1, amount do
+        get_entity(spawn(ENT_TYPE.ITEM_RUBBLE, x, y, l, 0, 0)).animation_frame = 54
+    end
+end
+
+local function bacterium_kill(bacterium)
+    local x, y, l = get_position(bacterium.uid)
+    spawn(ENT_TYPE.ITEM_BLOOD, x, y, l, math.random()*0.5-0.25, math.random()*0.25)
+    spawn(ENT_TYPE.ITEM_BLOOD, x, y, l, math.random()*0.5-0.25, math.random()*0.25)
+    spawn(ENT_TYPE.ITEM_BLOOD, x, y, l, math.random()*0.5-0.25, math.random()*0.25)
+    play_sound_at_entity(VANILLA_SOUND.ENEMIES_KILLED_ENEMY, bacterium.uid)
+    spawn_bacterium_rubble(x, y, l, 2)
+    bacterium:destroy()
+end
+
+---@param bacterium Frog
+---@param attacker Movable
+local function bacterium_damage(bacterium, attacker)
+    messpect(enum_get_name(ENT_TYPE, attacker.type.id))
+    if attacker.type.id == ENT_TYPE.ITEM_FREEZERAYSHOT then
+        if bacterium.frozen_timer == 0 then
+            spawn_over(ENT_TYPE.ITEM_ICECAGE, bacterium.uid, 0, 0)
+            bacterium.flags = set_flag(bacterium.flags, ENT_FLAG.CAN_BE_STOMPED)
+        end
+        bacterium.frozen_timer = 120
+        bacterium.stun_timer = 120
+        kill_entity(attacker.uid)
+        return true
+    elseif bacterium.frozen_timer == 0 then
+        if bacterium.stun_timer == 0 then
+            bacterium.stun_timer = 60
+        end
+        local x, y = get_position(bacterium.uid)
+        spawn(ENT_TYPE.ITEM_BLOOD, x, y, bacterium.layer, math.random()*0.5-0.25, math.random()*0.25)
+        bacterium.exit_invincibility_timer = 10
+        play_sound_at_entity(VANILLA_SOUND.TRAPS_STICKYTRAP_HIT, bacterium.uid)
+        return true
+    else
+        bacterium_kill(bacterium)
+    end
+end
+
+---@param ent Frog
 local function bacterium_set(ent)
-    ent.hitboxx = 0.45
-    ent.hitboxy = 0.45
+    ent.hitboxx, ent.hitboxy = 0.45, 0.45
+    ent.width, ent.height = 1.25, 1.25
     ent.offsety = 0
-    local x, y, l = get_position(ent.uid)
-    ent.owner_uid = get_solid_grid_entity(x, y-1, l)
-    messpect(ent.owner_uid)
+    ent:set_texture(bacterium_texture_id)
+    ent.health = 0
     ent.flags = set_flag(ent.flags, ENT_FLAG.NO_GRAVITY)
+    ent.flags = set_flag(ent.flags, ENT_FLAG.DEAD)
+    ent.flags = clr_flag(ent.flags, ENT_FLAG.TAKE_NO_DAMAGE)
     ent.flags = clr_flag(ent.flags, ENT_FLAG.COLLIDES_WALLS)
+    ent.flags = clr_flag(ent.flags, ENT_FLAG.INTERACT_WITH_SEMISOLIDS)
+    ent.flags = clr_flag(ent.flags, ENT_FLAG.INTERACT_WITH_WEBS)
+    set_on_damage(ent.uid, bacterium_damage)
+
+    local x, y, l = get_position(ent.uid)
+    messpect(get_solid_grid_entity(x, y-1, l))
     local is_inverse
     if math.random(2) == 1 then
         is_inverse = false
@@ -28,6 +116,7 @@ local function bacterium_set(ent)
         is_inverse = true
     end
     return {
+        attached_floor_uid = get_solid_grid_entity(x, y-1, l),
         inverse = is_inverse,
         dir_state = 2,
         movex = is_inverse and -1 or 1,
@@ -46,8 +135,8 @@ local function will_collide_floor(ent, ent_info)
 end
 
 local function will_be_out_of_owner(ent, ent_info)
-    local ox, oy = get_position(ent.owner_uid)
-    messpect(ent_info.movex, math.abs(ent.x + ent.velocityx - ent.hitboxx*ent_info.movex - ox), ent_info.movey, math.abs(ent.y + ent.velocityy - ent.hitboxy*ent_info.movey - oy))
+    local ox, oy = get_position(ent_info.attached_floor_uid)
+    --messpect(ent_info.movex, math.abs(ent.x + ent.velocityx - ent.hitboxx*ent_info.movex - ox), ent_info.movey, math.abs(ent.y + ent.velocityy - ent.hitboxy*ent_info.movey - oy))
     if (ent_info.movex ~= 0 and ent_info.movex*(ent.x + ent.velocityx - ent.hitboxx*ent_info.movex - ox) > 0.5)
     or (ent_info.movey ~= 0 and ent_info.movey*(ent.y + ent.velocityy - ent.hitboxy*ent_info.movey - oy) > 0.5)  then
         return true
@@ -60,25 +149,28 @@ local function update_move(ent, ent_info, ox, oy)
     ent.y = ent_info.movey == 0 and ent.y or oy + (ent.hitboxy+0.025) * 2 * ent_info.movey --+ ent.velocityy
     
     ent_info.movex, ent_info.movey = table.unpack(MOVE_MAP[ent_info.dir_state])
-    ent.velocityx = ent_info.movex*0.1
-    ent.velocityy = ent_info.movey*0.1
+    ent.velocityx = ent_info.movex*BACTERIUM_VEL
+    ent.velocityy = ent_info.movey*BACTERIUM_VEL
 end
 
+---comment
+---@param ent Movable
+---@param ent_info any
 local function bacterium_update(ent, ent_info)
-    if get_entity(ent.owner_uid) then
+    if get_entity(ent_info.attached_floor_uid) then
         if ent.stun_timer == 0 then
-            ent.velocityx = ent_info.movex*0.1
-            ent.velocityy = ent_info.movey*0.1
+            ent.velocityx = ent_info.movex*BACTERIUM_VEL
+            ent.velocityy = ent_info.movey*BACTERIUM_VEL
         else
             ent.velocityx = 0
-            ent.velocityy = 0
+            ent.velocityy = ent.frozen_timer == 0 and 0 or 0.01
         end
-        messpect(ent_info.dir_state, ent.owner_uid)
+        --messpect(ent_info.dir_state, ent_info.attached_floor_uid)
         if will_be_out_of_owner(ent, ent_info) then
-            local ox, oy, ol = get_position(ent.owner_uid)
+            local ox, oy, ol = get_position(ent_info.attached_floor_uid)
             local next_floor_uid = get_solid_grid_entity(ox+ent_info.movex, oy+ent_info.movey, ol)
             if next_floor_uid ~= -1 then
-                ent.owner_uid = next_floor_uid
+                ent_info.attached_floor_uid = next_floor_uid
             else
                 ent_info.dir_state = ent_info.inverse and (ent_info.dir_state - 2) % 4 + 1 or ent_info.dir_state % 4 + 1
                 update_move(ent, ent_info, ox, oy)
@@ -88,14 +180,14 @@ local function bacterium_update(ent, ent_info)
             if uid then
                 ent_info.dir_state = ent_info.inverse and ent_info.dir_state % 4 + 1 or (ent_info.dir_state - 2) % 4 + 1
                 ent_info.movex, ent_info.movey = ent_info.movex*-1, ent_info.movey*-1
-                ent.owner_uid = uid
+                ent_info.attached_floor_uid = uid
                 local ox, oy, ol = get_position(uid)
                 update_move(ent, ent_info, ox, oy)
             end
         end
     else
         if ent.standing_on_uid ~= -1 then
-            ent.owner_uid = ent.standing_on_uid
+            ent_info.attached_floor_uid = ent.standing_on_uid
             ent.standing_on_uid = -1
             ent.flags = set_flag(ent.flags, ENT_FLAG.NO_GRAVITY)
             ent.flags = clr_flag(ent.flags, ENT_FLAG.COLLIDES_WALLS)
@@ -104,9 +196,25 @@ local function bacterium_update(ent, ent_info)
             ent.flags = set_flag(ent.flags, ENT_FLAG.COLLIDES_WALLS)
         end
     end
+    if ent.frozen_timer == 0 then
+        if ent:is_on_fire() then
+            bacterium_kill(ent)
+        end
+        for _,p_uid in ipairs(get_entities_overlapping_hitbox(0, MASK.PLAYER, get_hitbox(ent.uid), LAYER.PLAYER)) do
+            local player = get_entity(p_uid)
+            if player.invincibility_frames_timer == 0 then
+                local x = get_position(ent.uid)
+                local px = get_position(p_uid)
+                player:damage(ent.uid, 1, 0, px > x and 0.1 or -0.1, 0.1, 60)
+            end
+            bacterium_kill(ent)
+        end
+        ent.animation_frame = math.floor(ent.idle_counter / 5)
+        ent.flags = clr_flag(ent.flags, ENT_FLAG.CAN_BE_STOMPED)
+    end
 end
 
-local bacterium_id = celib.new_custom_entity(bacterium_set, bacterium_update)
+local bacterium_id = celib.new_custom_entity(bacterium_set, bacterium_update, nil, ENT_TYPE.ITEM_ROCK, celib.UPDATE_TYPE.POST_STATEMACHINE)
 
 celib.init()
 
--- lib/entities/giant_frog.lua
+++ lib/entities/giant_frog.lua
@@ -1,42 +1,90 @@
-local celib = require "custom-entities"
+local celib = require "custom_entities"
+local logger = require "logger"
 
 --giant frogs spit only 5 frogs and then only jump (?)
-local function giant_frog_set()
-
+---@param ent Frog
+local function giant_frog_set(ent)
+    ent.health = 10
+    ent.hitboxx, ent.hitboxy = 0.640, 0.700
+    ent.width, ent.height = 2.0, 2.0
+    ent.flags = clr_flag(ent.flags, ENT_FLAG.CAN_BE_STOMPED)
     return {
-        spitting = false
+        spitting = false,
+        script_jumped = false
     }
 end
 
-local function giant_frog_jump(ent)
+local function face_target(mons_uid, target_uid)
+    local mx = get_position(mons_uid)
+    local tx = get_position(target_uid)
+    if tx-mx < 0 then
+        set_entity_flags(mons_uid, set_flag(get_entity_flags(mons_uid), ENT_FLAG.FACING_LEFT))
+    else
+        set_entity_flags(mons_uid, clr_flag(get_entity_flags(mons_uid), ENT_FLAG.FACING_LEFT))
+    end
+end
 
+local function giant_frog_jump(ent)
+    local vel_x = test_flag(ent.flags, ENT_FLAG.FACING_LEFT) and -0.0525 or 0.0525
+    ent.velocityx = vel_x
+    ent.velocityy = 0.175
 end
 
 local function giant_frog_spit(ent)
+    local facing_left = test_flag(ent.flags, ENT_FLAG.FACING_LEFT)
     local x, y, l = get_position(ent.uid)
-    ent.animation_frame = 0
-    local vx = test_flag(ent.flags, ENT_FLAG.FACING_LEFT) and -1 or 1
-    get_entity(spawn(ENT_TYPE.MONS_FROG, x+vx, y+0.1, l, vx*0.2, 0.1)).last_owner_uid = ent.uid
+    --ent.animation_frame = 0
+    local vx = facing_left and -0.120 or 0.120
+    local spawned = get_entity(spawn(ENT_TYPE.MONS_FROG, x+vx, y+0.1, l, vx, 0.1025))
+    spawned.last_owner_uid = ent.uid
+    if facing_left then
+        spawned.flags = set_flag(spawned.flags, ENT_FLAG.FACING_LEFT)
+    else
+        spawned.flags = clr_flag(spawned.flags, ENT_FLAG.FACING_LEFT)
+    end
     ent.idle_counter = 0
 end
 
+---comment
+---@param ent Monster
+---@param c_data any
 local function giant_frog_update(ent, c_data)
-    if ent.standing_on_uid and ent.state ~= CHAR_STATE.JUMPING then
+    if ent.standing_on_uid ~= -1 and ent.state ~= CHAR_STATE.JUMPING then
+        c_data.script_jumped = false
         if ent.jump_timer == 0 then
+            face_target(ent.uid, ent.chased_target_uid)
             if math.random(2) == 1 then
                 giant_frog_jump(ent)
             else
                 giant_frog_spit(ent)
                 c_data.spitting = true
             end
-            ent.jump_timer = math.random(120, 300)
+            local time = math.random(180, 255)
+            ent.jump_timer = time
+            messpect(time)
+            c_data.script_jumped = true
         else
             if c_data.spitting then
-                ent.animation_frame = math.floor(ent.idle_counter / 2)
-                if ent.animation_frame > 4 then
+                --ent.animation_frame = math.floor(ent.idle_counter / 2)
+                if ent.idle_counter > 8 then
                     c_data.spitting = false
                 end
             end
         end
+    elseif not c_data.script_jumped then
+        ent.velocityx = math.min(math.abs(ent.velocityx), math.abs(0.035)) * (ent.velocityx > 0 and 1 or -1)
+        ent.velocityy = math.min(ent.velocityy, 0)
     end
+    logger.log_text_uid(tostring(c_data.script_jumped), ent.uid)
+    logger.log_text_uid(tostring(c_data.spitting), ent.uid)
+    logger.log_text_uid(tostring(ent.jump_timer), ent.uid)
 end
+
+local giant_frog_id = celib.new_custom_entity(giant_frog_set, giant_frog_update, nil, ENT_TYPE, celib.UPDATE_TYPE.POST_STATEMACHINE)
+
+local function spawn_frog()
+    local x, y, l = get_position(players[1].uid)
+    celib.set_custom_entity(spawn(ENT_TYPE.MONS_FROG, x+2, y, l, 0, 0), giant_frog_id)
+end
+
+register_option_button("spawn_frog", "spawn g frog", "", spawn_frog)

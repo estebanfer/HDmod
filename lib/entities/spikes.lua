0c44b624284edffdfa27ee0455dbaf8d59172ac4
SuperNinjaFat <paulmlindberg@gmail.com>
2022-09-27 16:06:50 -0500

    clean up spikes creation method

--- lib/entities/spikes.lua
+++ lib/entities/spikes.lua
@@ -3,8 +3,16 @@ local commonlib = require 'lib.common'
 local module = {}
 
 local vlad_spikes = {}
+local wood_spikes_texture_id
+local worm_spikes_texture_id
 local vlad_spikes_texture_id
 do
+    local wood_spikes_texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOOR_CAVE_0)
+    wood_spikes_texture_def.texture_path = "res/wood_spikes.png"
+    wood_spikes_texture_id = define_texture(wood_spikes_texture_def)
+
+    worm_spikes_texture_id = define_texture(get_texture_definition(TEXTURE.DATA_TEXTURES_FLOOR_EGGPLANT_0))
+
     local vlad_spikes_texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOOR_VOLCANO_0)
     vlad_spikes_texture_def.texture_path = "res/vladspikes.png"
     vlad_spikes_texture_id = define_texture(vlad_spikes_texture_def)
@@ -18,40 +26,27 @@ function module.create_spikes_over(floor_uid)
     ---@type Floor
     local floor = get_entity(floor_uid)
     local spikes_uid = spawn_entity_over(ENT_TYPE.FLOOR_SPIKES, floor_uid, 0, 1)
-    if (
+    local wood_in_mines = (
         state.theme == THEME.DWELLING
         and floor.type.id == ENT_TYPE.FLOORSTYLED_MINEWOOD
+    )
+    if (
+        wood_in_mines
+        or state.theme == THEME.EGGPLANT_WORLD
     ) then
-        local texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOOR_CAVE_0)
-        texture_def.texture_path = "res/wood_spikes.png"
-        local deco_texture = define_texture(texture_def)
-
-        floor:add_decoration(FLOOR_SIDE.TOP)
-        
-        if floor.deco_top ~= -1 then
-            local deco = get_entity(floor.deco_top)
-            deco:set_texture(deco_texture)
-            deco.animation_frame = math.random(101, 103)
+        local deco_texture = wood_spikes_texture_id
+        if state.theme == THEME.EGGPLANT_WORLD then
+            deco_texture = worm_spikes_texture_id
         end
-    elseif state.theme == THEME.EGGPLANT_WORLD then
-        local texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOOR_EGGPLANT_0)
-        local deco_texture = define_texture(texture_def)
-
         floor:add_decoration(FLOOR_SIDE.TOP)
-        
         if floor.deco_top ~= -1 then
             local deco = get_entity(floor.deco_top)
             deco:set_texture(deco_texture)
             deco.animation_frame = math.random(101, 103)
         end
     elseif state.theme == THEME.VOLCANA then
-        -- need subchunkid of what room we're in
-        local roomx, roomy = locatelib.locate_levelrooms_position_from_game_position(x, y)
-        local _subchunk_id = roomgenlib.global_levelassembly.modification.levelrooms[roomy][roomx]
         if (
-            _subchunk_id == roomdeflib.HD_SUBCHUNKID.VLAD_TOP
-            or _subchunk_id == roomdeflib.HD_SUBCHUNKID.VLAD_MIDSECTION
-            or _subchunk_id == roomdeflib.HD_SUBCHUNKID.VLAD_BOTTOM
+            get_entity_type(floor_uid) == ENT_TYPE.FLOORSTYLED_VLAD
         ) then
             get_entity(spikes_uid):set_texture(vlad_spikes_texture_id)
             vlad_spikes[#vlad_spikes+1] = spikes_uid

5a37dd916925d235c6986005d8f5f0a21ef68f83
Super Ninja Fat <paulmlindberg@gmail.com>
2023-04-01 00:34:51 -0400

    Implement turning rock into snowball
    TODO: turn into a rock upon hitting floors.

--- lib/entities/snowball.lua
+++ lib/entities/snowball.lua
@@ -1,16 +1,43 @@
 local module = {}
 local celib = require "lib.entities.custom_entities"
 local feelingslib = require "lib.feelings"
---[[ TODO:
-    2. When the snowing level feeling occurs, replace all rocks in a level with this entity.
-    3. Replace the rock's texture with the snowball texture
-    4. When the rock damages something it looses its snowball texture and emits snow particles
-]]
+local commonlib = require "lib.common"
+--#TODO: turn into a rock upon hitting floors 
 
 celib.init()
 
+local function spawn_snowball_rubble(x, y, l, amount)
+    for _=1, amount do
+        get_entity(spawn(ENT_TYPE.ITEM_RUBBLE, x, y, l, 0, 0)).animation_frame = 6
+    end
+end
+
+---@param ent Entity
+local function turn_into_rock(ent)
+    ent.animation_frame = 16
+    ent.flags = set_flag(ent.flags, ENT_FLAG.TAKE_NO_DAMAGE)
+    local x, y, l = get_position(ent.uid)
+    spawn_snowball_rubble(x, y, l, 5)
+    commonlib.play_sound_at_entity(VANILLA_SOUND.CRITTERS_DRONE_CRASH, ent.uid)
+end
+
+---@param self Entity
+local function check_for_damage(self, damage_dealer, damage_amount, velocity_x, velocity_y, stun_amount, iframes)
+    if (self.animation_frame ~= 16) then
+        turn_into_rock(self)
+    end
+end
+
+---@param ent Movable
 local function snowball_set(ent)
     ent.animation_frame = 222
+    ent.flags = clr_flag(ent.flags, ENT_FLAG.TAKE_NO_DAMAGE)
+    set_on_damage(ent.uid, check_for_damage)
+    ent:set_pre_on_collision2(function (self, other_entity)
+        if (self.animation_frame ~= 16 and (self.velocityx >= math.abs(0.1) or self.velocityy >= math.abs(0.1))) then
+            turn_into_rock(self)
+        end
+    end)
 end
 
 local snowball_id = celib.new_custom_entity(snowball_set, function() end, celib.CARRY_TYPE.HELD, ENT_TYPE.ITEM_ROCK)

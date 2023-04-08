34fcf493318bc0a862db4d3e5a472ac91f91eccb
erictran4011 <103012990+erictran4011@users.noreply.github.com>
2023-04-08 17:13:54 -0400

    Alien lord revision
    Fixed up some problems with the Alien Lord, more to come.

--- lib/entities/alienlord.lua
+++ lib/entities/alienlord.lua
@@ -15,22 +15,58 @@ end
 
 local function alienlord_update(ent)
     ent.x, ent.y = ent.spawn_x, ent.spawn_y
-    -- if ent.walk_pause_timer > 200 then
-    --     ent.animation_frame = 0
-    -- elseif ent.walk_pause_timer <= 200 and ent.walk_pause_timer > 100 then
-    --     ent.animation_frame = 1
-    -- elseif ent.walk_pause_timer <= 100 and ent.walk_pause_timer > 0 then
-    --     ent.animation_frame = 2
-    -- elseif ent.walk_pause_timer <= 0 then
-    --     ent.animation_frame = 3
-    --     ent.walk_pause_timer = 300
-    -- end
+    -- Increase animation timer
+    ent.user_data.animation_timer = ent.user_data.animation_timer + 1
+    --- Animate the entity and reset the timer
+    if ent.user_data.animation_timer >= ent.user_data.animation_speed then
+        ent.user_data.animation_timer = 1
+        -- Advance the animation
+        ent.user_data.animation_frame = ent.user_data.animation_frame + 1
+        -- Loop if the animation has reached the end
+        if ent.user_data.animation_frame > ent.user_data.animation_end then
+            ent.user_data.animation_frame = ent.user_data.animation_start
+        end
+    end
+    -- Change the actual animation frame
+    ent.animation_frame = ent.user_data.animation_frame
+    -- Update the volume and panning of the mushy alien noises
+    if ent.user_data.alien_sound ~= nil then
+        commonlib.update_sound_volume(ent.user_data.alien_sound, ent.uid, 0.75)
+    end
+    -- Stop the base entity from attacking
+    ent.next_attack_timer = 5
+    -- Our own attack timer and spawning system
+    if ent.lock_input_timer == 0 and ent.frozen_timer == 0 and ent.stun_timer == 0 then
+        -- Ensure the player is in range first
+        if players[1] ~= nil then
+            if distance(players[1].uid, ent.uid) < 8.5 then
+                ent.user_data.attack_timer = ent.user_data.attack_timer - 1
+                -- Update the animation speed to reflect this
+                ent.user_data.animation_speed = 5
+            else
+                ent.user_data.animation_speed = 7
+            end
+        end
+    end
+    -- Perform an attack
+    if ent.user_data.attack_timer <= 0 then
+        -- Spawn the shot
+        local x, y, l = get_position(ent.uid)
+        local lor = 1
+        if test_flag(ent.flags, ENT_FLAG.FACING_LEFT) then lor = -1 end
+        local atk = get_entity(spawn(ENT_TYPE.ITEM_SCEPTER_ANUBISSHOT, x+(0.5*lor), y+0.15, l, 0, 0))
+        atk.last_owner_uid = ent.uid
+        -- Reset the attack timer
+        ent.user_data.attack_timer = ent.user_data.attack_timer_base
+    end
 end
 
 local function alienlord_set(uid)
     ---@type Movable
     local ent = get_entity(uid)
     ent.move_state = 5
+    -- Set health
+    ent.health = 10
 	local roomx, roomy = locatelib.locate_levelrooms_position_from_game_position(ent.x, ent.y)
 	local _subchunk_id = locatelib.get_levelroom_at(roomx, roomy)
     if _subchunk_id == roomdeflib.HD_SUBCHUNKID.MOTHERSHIP_ALIENLORD_LEFT then
@@ -39,11 +75,56 @@ local function alienlord_set(uid)
     -- user_data
     ent.user_data = {
         ent_type = HD_ENT_TYPE.MONS_ALIENLORD;
+        alien_sound = nil; -- The alien queen in vanilla S2 makes these gross squishy noises, I think they'd fit this entity as well
+
+        -- ANIMATION
+        animation_timer = 1;
+        animation_speed = 5; -- Number of game frames before the animation_frame is increased
+        animation_start = 0; -- What frame the animation goes back to before looping
+        animation_end = 3; -- When the animation is this number, it will loop back to the start instead of increasing animation_frame
+        animation_frame = 0; -- We need our own animation frame because the actual animation frame keeps getting changed by the statemachine
+
+        -- ATTACK
+        attack_timer = 150;
+        attack_timer_base = 150; -- Not sure what the HD value is but we can just change it in the future
     };
-    -- ent.walk_pause_timer = 300
+    ent.animation_frame = ent.user_data.animation_start
+    -- Play the squishy alien noises
+    ent.user_data.alien_sound = commonlib.play_sound_at_entity(VANILLA_SOUND.ENEMIES_ALIEN_QUEEN_LOOP, ent.uid, 0.75)
+    -- Change the texture to our custom one
     set_timeout(function()
         ent:set_texture(alienlord_texture_id)
     end, 2)
+    -- Stop the gross alien slime noises before death and move this entity out of bounds so we don't get the scepter drop
+    ent:set_pre_kill(function()
+        if ent.user_data.alien_sound ~= nil then
+            ent.user_data.alien_sound:stop()
+        end
+        local x, y, l = get_position(ent.uid)
+        -- Gibs and blood
+        for _=1, 8 do
+            local rubble = get_entity(spawn(ENT_TYPE.ITEM_RUBBLE, x+math.random(-10, 10)/100, y+math.random(-10, 10)/100, l, math.random(-15, 15)/100, math.random(10, 15)/100))
+            rubble.animation_frame = 42
+        end
+        for _=1, 10 do
+            local rubble = get_entity(spawn(ENT_TYPE.ITEM_BLOOD, x+math.random(-10, 10)/100, y+math.random(-10, 10)/100, l, math.random(-15, 15)/100, math.random(10, 15)/100))
+        end
+        -- Gems
+       spawn(ENT_TYPE.ITEM_RUBY, x, y, l, math.random(-15, 15)/100, math.random(10, 15)/100)
+       spawn(ENT_TYPE.ITEM_SAPPHIRE, x, y, l, math.random(-15, 15)/100, math.random(10, 15)/100)
+       -- Jetpack
+       spawn(ENT_TYPE.ITEM_JETPACK, x, y-0.25, l, 0, 0)
+        -- Sfx
+        commonlib.play_sound_at_entity(VANILLA_SOUND.ENEMIES_KILLED_ENEMY, ent.uid, 1)
+        -- Move base entity out of bounds
+        ent.x = -900
+    end)
+    -- We should stop them too if this entity ever gets unloaded from memory, like if you ever restart near it
+    ent:set_pre_dtor(function()
+        if ent.user_data.alien_sound ~= nil then
+            ent.user_data.alien_sound:stop()
+        end
+    end)
 end
 
 set_post_entity_spawn(function(ent)
@@ -64,7 +145,7 @@ end, SPAWN_TYPE.ANY, MASK.ANY, ENT_TYPE.ITEM_SCEPTER_ANUBISSHOT)
     -- Change projectile speed with `ScepterShot::speed`
 --]]
 function module.create_alienlord(x, y, l)
-    local alienlord = spawn(ENT_TYPE.MONS_ANUBIS, x+.5, y-.5, l, 0, 0)
+    local alienlord = spawn(ENT_TYPE.MONS_ANUBIS, x+.5, y-.65, l, 0, 0)
     alienlord_set(alienlord)
     set_post_statemachine(alienlord, alienlord_update)
     return alienlord

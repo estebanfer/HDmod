30b0b851606cd0134c8a145971e295f5c8c4c0bd
Super Ninja Fat <paulmlindberg@gmail.com>
2023-04-14 19:58:32 -0400

    Set the demo world to 3

--- lib/demo.lua
+++ lib/demo.lua
@@ -1,6 +1,6 @@
 local module = {}
 
-module.DEMO_MAX_WORLD = 2
+module.DEMO_MAX_WORLD = 3
 optionslib.register_option_bool("hd_debug_demo_enable_all_worlds", "Demo - Enable unfinished worlds", nil, false, true)
 optionslib.register_option_bool("hd_debug_demo_enable_tutorial", "Demo - Enable unfinished tutorial", nil, false, true)
 

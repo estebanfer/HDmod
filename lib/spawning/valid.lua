6e5082c44bdcbb37f0472c318bff4f95d5169ae1
Super Ninja Fat <paulmlindberg@gmail.com>
2023-04-13 21:55:18 -0400

    Fix ufo valid spawn params from previous commit

--- lib/spawning/valid.lua
+++ lib/spawning/valid.lua
@@ -687,7 +687,7 @@ function module.is_valid_ufo_spawn(x, y, l)
 	return (
 		-- HD also avoids the coffin rooms here, but I think the API already accounts for that since we set S2 coffin rooms
 		room ~= roomdeflib.HD_SUBCHUNKID.MOTHERSHIP_ALIENQUEEN
-		and default_ceiling_entity_condition
+		and default_ceiling_entity_condition(x, y, l)
 	)
 end
 

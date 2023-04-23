532f91faa3186ea2e79e1eabad3a43b068132da4
Super Ninja Fat <paulmlindberg@gmail.com>
2023-04-22 20:56:38 -0400

    Space-optimize castledoor.png
    Simplify sliding_wall library texture variable names

--- lib/entities/sliding_wall.lua
+++ lib/entities/sliding_wall.lua
@@ -3,22 +3,26 @@ feelingslib = require 'lib.feelings'
 
 local module = {}
 local hc_sliding_wall_ceiling = nil
-local temple_chain_ceiling_texture_id
+local temple_chain_texture_id
 local temple_slidingdoor_texture_id
-local hc_slidingdoor_texture_id
+local hc_texture_id
 
 do
-    local temple_chain_ceiling_texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOORSTYLED_PAGODA_0)
-    temple_chain_ceiling_texture_def.texture_path = "res/slidingwall_temple.png"
-    temple_chain_ceiling_texture_id = define_texture(temple_chain_ceiling_texture_def)
+    local temple_chain_texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOORSTYLED_PAGODA_0)
+    temple_chain_texture_def.texture_path = "res/slidingwall_temple.png"
+    temple_chain_texture_id = define_texture(temple_chain_texture_def)
     
     local temple_slidingdoor_texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOORSTYLED_PAGODA_1)
     temple_slidingdoor_texture_def.texture_path = "res/slidingwall_temple.png"
     temple_slidingdoor_texture_id = define_texture(temple_slidingdoor_texture_def)
     
-    local hc_slidingdoor_texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_FLOORSTYLED_PAGODA_1)
-    hc_slidingdoor_texture_def.texture_path = "res/castledoor.png"
-    hc_slidingdoor_texture_id = define_texture(hc_slidingdoor_texture_def)
+    local hc_texture_def = TextureDefinition.new()
+	hc_texture_def.width = 128
+	hc_texture_def.height = 256
+	hc_texture_def.tile_width = 128
+	hc_texture_def.tile_height = 256
+    hc_texture_def.texture_path = "res/castledoor.png"
+    hc_texture_id = define_texture(hc_texture_def)
 end
 
 function module.spawn_slidingwall(x, y, layer, up)
@@ -35,13 +39,13 @@ function module.spawn_slidingwall(x, y, layer, up)
     if state.theme == THEME.TEMPLE then
         idollib.sliding_wall_ceilings[#idollib.sliding_wall_ceilings+1] = ceiling.uid
 
-        ceiling:set_texture(temple_chain_ceiling_texture_id)
-        chain:set_texture(temple_chain_ceiling_texture_id)
+        ceiling:set_texture(temple_chain_texture_id)
+        chain:set_texture(temple_chain_texture_id)
         wall:set_texture(temple_slidingdoor_texture_id)
     end
 
     if feelingslib.feeling_check(feelingslib.FEELING_ID.HAUNTEDCASTLE) then
-        wall:set_texture(hc_slidingdoor_texture_id)
+        wall:set_texture(hc_texture_id)
 
         hc_sliding_wall_ceiling = ceiling.uid
 
@@ -69,7 +73,7 @@ end
 
 set_post_entity_spawn(function(entity)
     if state.theme == THEME.TEMPLE then
-        entity:set_texture(temple_chain_ceiling_texture_id)
+        entity:set_texture(temple_chain_texture_id)
     end
 end, SPAWN_TYPE.ANY, MASK.ANY, ENT_TYPE.ITEM_SLIDINGWALL_CHAIN)
 

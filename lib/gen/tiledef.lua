d215bfb344e655c9ed0104e88b83a6f9883409e3
Super Ninja Fat <paulmlindberg@gmail.com>
2023-05-30 00:21:44 -0400

    Move altar methods into their own library

--- lib/gen/tiledef.lua
+++ lib/gen/tiledef.lua
@@ -20,6 +20,7 @@ local ladderlib = require 'lib.entities.ladder'
 local succubuslib = require 'lib.entities.succubus'
 local coffinlib = require 'lib.entities.coffin'
 local liquidfalllib = require 'lib.entities.liquidfall'
+local altarlib = require 'lib.entities.altar'
 
 local module = {}
 
@@ -1051,29 +1052,7 @@ module.HD_TILENAME = {
 					end,
 				},
 				alternate = {
-					[THEME.JUNGLE] = {
-						function(x, y, l)
-							local ent_uid = spawn_entity(ENT_TYPE.BG_BASECAMP_SHORTCUTSTATIONBANNER, x+4, y+2, l, 0, 0)
-							local texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_DECO_BASECAMP_3)
-							texture_def.texture_path = "res/hauntedcastle_banner.png"
-							get_entity(ent_uid):set_texture(define_texture(texture_def))
-							
-							ent_uid = spawn_entity(ENT_TYPE.BG_KALI_STATUE, x+.5, y+0.6, l, 0, 0)
-							local texture_def = get_texture_definition(TEXTURE.DATA_TEXTURES_DECO_JUNGLE_0)
-							texture_def.texture_path = "res/hauntedcastle_deco.png"
-							get_entity(ent_uid):set_texture(define_texture(texture_def))
-							get_entity(ent_uid).width = 5.0--5.600
-							get_entity(ent_uid).height = 5.0--7.000
-
-							spawn_grid_entity(ENT_TYPE.FLOOR_ALTAR, x, y-1, l)
-							spawn_grid_entity(ENT_TYPE.FLOOR_ALTAR, x+1, y-1, l)
-
-							spawn_grid_entity(ENT_TYPE.FLOORSTYLED_STONE, x-1, y, l)
-							spawn_grid_entity(ENT_TYPE.FLOORSTYLED_STONE, x-1, y-1, l)
-							spawn_grid_entity(ENT_TYPE.FLOORSTYLED_STONE, x+2, y, l)
-							spawn_grid_entity(ENT_TYPE.FLOORSTYLED_STONE, x+2, y-1, l)
-						end,
-					}
+					[THEME.JUNGLE] = { altarlib.create_hc_altar }
 				},
 			}
 		},
@@ -1315,12 +1294,7 @@ module.HD_TILENAME = {
 	["x"] = {
 		phases = {
 			[1] = {
-				default = {
-					function(x, y, l)
-						spawn_grid_entity(ENT_TYPE.FLOOR_ALTAR, x, y, l)
-						spawn_grid_entity(ENT_TYPE.FLOOR_ALTAR, x+1, y, l)
-					end,
-				},
+				default = { altarlib.create_altar },
 			}
 		},
 		description = "Kali Altar",

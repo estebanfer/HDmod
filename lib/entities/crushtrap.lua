791be388928a83d0ca5a0342ad8908f49b2ad6fc
Super Ninja Fat <paulmlindberg@gmail.com>
2023-06-03 17:34:28 -0400

    Implement crushtrap spawns
    Move crushtrap creation into its own library
    Apply stone crushtrap texture

---
+++ lib/entities/crushtrap.lua
@@ -0,0 +1,21 @@
+local module = {}
+
+local texture_id
+do
+    local texture_def = TextureDefinition.new()
+    texture_def.width = 128
+    texture_def.height = 128
+    texture_def.tile_width = 128
+    texture_def.tile_height = 128
+    texture_def.texture_path = "res/crushtrap_stone.png"
+    texture_id = define_texture(texture_id)
+end
+
+function module.create_crushtrap(x, y, l)
+    local crushtrap = get_entity(spawn_grid_entity(ENT_TYPE.ACTIVEFLOOR_CRUSH_TRAP, x, y, l))
+    if options.hd_og_floorstyle_temple then
+        crushtrap:set_texture(texture_id)
+    end
+end
+
+return module

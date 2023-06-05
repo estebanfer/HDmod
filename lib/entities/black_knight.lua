4db3d2fc0868756a4271059bbd1f85ae29d18495
Super Ninja Fat <paulmlindberg@gmail.com>
2023-06-04 23:32:53 -0400

    Add new black knight sprites
    Remove erroneously placed black knight texture

--- lib/entities/black_knight.lua
+++ lib/entities/black_knight.lua
@@ -36,11 +36,11 @@ local ANIMATION_FRAMES_BASE = {
 local ANIMATION_FRAMES_RES = {
     { 0 },
     { 1, 2, 3, 4, 5, 6, 7, 8 },
+    { 9 },
     { 10 },
-    { 15 },
-    { 16 },
-    { 17 },
-    { 18 },
+    { 11 },
+    { 12 },
+    { 13 },
 }
 
 local function black_knight_set(uid)
